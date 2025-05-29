323d0a1420e33adde8918a7e31c30c472041e5bf
Justin <1087405@hr.nl>
2025-05-29 19:46:05 +0200

    Resolution

--- src/js/game.js
+++ src/js/game.js
@@ -1,16 +1,16 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import {Actor, Engine, Vector, DisplayMode} from "excalibur"
+import {Resources, ResourceLoader} from './resources.js'
 
 export class Game extends Engine {
 
     constructor() {
-        super({ 
-            width: 1280,
-            height: 720,
+        super({
+            width: 800,
+            height: 600,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
-         })
+        })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
@@ -19,7 +19,7 @@ export class Game extends Engine {
         const fish = new Actor()
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
+        fish.vel = new Vector(-10, 0)
         fish.events.on("exitviewport", (e) => this.fishLeft(e))
         this.add(fish)
     }
